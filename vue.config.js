64917a331c96b0ad96c1c6ce5b1617c6981064e9
asddsa356 <asddsa356@mail.ru>
2022-11-02 23:08:19 +0300

    fix public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/testworkwithoutVuex/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/testworkwithoutVuex/dist/" : "/",
   transpileDependencies: true,
   configureWebpack: {
     devtool: 'source-map',
